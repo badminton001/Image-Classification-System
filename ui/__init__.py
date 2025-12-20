99d8da162a37f34abf308b49e9df85b0906b6101
tao3693 <taoouwen@foxmail.com>
2025-12-20 15:42:14 +0800

    Update exports in UI package for image classifier

--- ui/__init__.py
+++ ui/__init__.py
@@ -2,7 +2,6 @@
 UI package for the Image Classification Enhancement System.
 """
 
-from .interface import main_menu
+from .interface import ImageClassifierApp, launch_gui, main_menu
 
-__all__ = ["main_menu"]
-
+__all__ = ["ImageClassifierApp", "launch_gui", "main_menu"]
